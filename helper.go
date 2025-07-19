5b1b88a91c9570b7e63f8ba55b8ff659fb550c27
Tim-Restart <tim_restart@proton.me>
2025-07-19 14:47:12 +1000

    basic cmds and file names complete

---
+++ helper.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func (files *fileDetails) testPrint() {
+	fmt.Printf(`
+===================================
+File name to be searched: %v
+Number of files to be searched: %v
+===================================
+	`, files.fileName, files.numberOfFiles)
+}
+
+func (files *fileDetails) createFileName() []string {
+	// Function to create the file name to parse to the open file command
+	// This will check if it is a single file, or a sequential file
+	// Logic for this function will likely just deal with single files
+	// Second help function to be callled in this one to deal with multiple files?
+
+	// Initalise a slice of strings to load the file names into
+	var fileSlice []string
+
+	if files.numberOfFiles != 0 {
+		// Logic here for multiple files
+		for i := 0; i < files.numberOfFiles; i++ {
+			if i == 0 {
+				// Edgecase for the first html document, no numbers appended
+				fileSlice = append(fileSlice, files.fileName+fileExt)
+			} else {
+				indexed := i + 1
+				n := strconv.Itoa(indexed)
+				fileSlice = append(fileSlice, files.fileName+n+fileExt)
+			}
+		}
+		return fileSlice
+	} else {
+		fileSlice = append(fileSlice, files.fileName+fileExt)
+		return fileSlice
+	}
+}
+
+/*
+func (files *fileDetails) openFile(file string)
+
+file, err := os.Open(file) // For read access.
+if err != nil {
+	log.Fatal(err)
+}
+*/
--- main.go
+++ main.go
@@ -4,23 +4,22 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	)
+)
 
+const fileExt = ".html"
 
-func main(){
+type fileDetails struct {
+	fileName      string
+	numberOfFiles int
+}
 
-	const fileExt = ".html"
-
-	type fileDetails struct {
-		fileName			string
-		numberOfFiles		int
-	}
+func main() {
 
 	// Declared empty file details to be completed in the swtich statement below
-	// If no number of files supplied, go off on a different fork to complete only one 
+	// If no number of files supplied, go off on a different fork to complete only one
 	// Likely also move the swtich statement to a clean file and call form the main
 
-	files := fileDetails{}
+	files := &fileDetails{}
 
 	switch len(os.Args) {
 	case 1:
@@ -29,20 +28,25 @@ func main(){
 	case 2:
 		fmt.Printf("starting Parse of: %v\n", os.Args[1])
 		files.fileName = os.Args[1]
-		fmt.Println("Single file being parsed: %v%v", files.fileName, fileExt)
+		fmt.Printf("Single file being parsed: %v%v", files.fileName, fileExt)
 	case 3:
 		fmt.Printf("starting crawl of: %v\n", os.Args[1])
 		files.fileName = os.Args[1]
-		files.numberOfFiles, _ := strconv.Atoi(os.Args[2])
-		/*
+		intConv, err := strconv.Atoi(os.Args[2])
 		if err != nil {
-			fmt.Println("Error converting input to int")
+			fmt.Println("Error converting string to int")
 			return
 		}
+		files.numberOfFiles = intConv
+		/*
+			if err != nil {
+				fmt.Println("Error converting input to int")
+				return
+			}
 		*/
-		fmt.Printf("File name: %v\n Number of files to be parsed: %v", files.fileName, files.numberOfFiles)
-		
-	/* Concurrency logic - yet to be implemented	
+		fmt.Printf("File name: %v\nNumber of files to be parsed: %v", files.fileName, files.numberOfFiles)
+
+	/* Concurrency logic - yet to be implemented
 	case 4:
 		fmt.Printf("starting crawl of: %v\n", os.Args[1])
 		website = os.Args[1]
@@ -51,11 +55,15 @@ func main(){
 		maxPagesSet, _ = strconv.Atoi(os.Args[3])
 		fmt.Printf("Max pages set to : %v\n", maxPagesSet)
 
-	*/ 
-	
+	*/
+
 	default:
 		fmt.Println("Failed to set right parameters")
 		return
 	}
+
+	files.testPrint()
+	hosp := files.createFileName()
+	fmt.Println(hosp)
 
 }
